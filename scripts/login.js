50c0db63462a31b0100bb7267fad713f62f56278
Eric Chen <75589433+EricChen23@users.noreply.github.com>
2022-01-16 04:34:26 -0800

    login.js mod

--- scripts/login.js
+++ scripts/login.js
@@ -22,3 +22,33 @@ document.addEventListener("DOMContentLoaded", ()=> {
     });
 });
 
+document.getElementById("register").addEventListener("click", () => {
+    const username = document.getElementById("newUsername").value;
+    const email = document.getElementById("newEmail").value;
+    const password = document.getElementById("newPassword").value;
+    const confirmPassword = document.getElementById("confirmPassword").value;
+    if(password != confirmPassword){
+        
+    }
+    fetch("https://jobysseyapi.herokuapp.com/api/v1/company/".concat(username, "/", password)).then(response=>{
+        return response.json();
+    }).then(json=>{
+        if(json['username'] != "fake"){
+            window.location.replace("../html/index.html");
+        }
+    })
+});
+
+document.getElementById("loginButton").addEventListener("click", () => {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    fetch("https://jobysseyapi.herokuapp.com/api/v1/company/".concat(username, "/", password)).then(response=>{
+        return response.json();
+    }).then(json=>{
+        if(json['username'] != "fake"){
+            window.location.replace("../html/index.html");
+        } else {
+            document.getElementById("loginError").classList.remove("hide");
+        }
+    })
+});
