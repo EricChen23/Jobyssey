1616af8e704e776c544cd4d8cb78a43e9a978b16
Danny Hu <danny2168h@gmail.com>
2022-01-16 10:13:22 -0800

    Added registration

--- scripts/login.js
+++ scripts/login.js
@@ -23,20 +23,36 @@ document.addEventListener("DOMContentLoaded", ()=> {
 });
 
 document.getElementById("register").addEventListener("click", () => {
+    event.preventDefault();
     const username = document.getElementById("newUsername").value;
     const email = document.getElementById("newEmail").value;
     const password = document.getElementById("newPassword").value;
     const confirmPassword = document.getElementById("confirmPassword").value;
-    if(password != confirmPassword){
-        
-    }
+    if(password.localeCompare(confirmPassword) != 0){
+        document.getElementById("registerError").classList.remove("hide");
+    } else {
     fetch("https://jobysseyapi.herokuapp.com/api/v1/company/".concat(username, "/", password)).then(response=>{
         return response.json();
     }).then(json=>{
-        if(json['username'] != "fake"){
+        if(json['username'] == "fake"){
+
+            var str = "https://jobysseyapi.herokuapp.com/api/v1/company/user/";
+
+            let data = {name: username, password: password, email: email, applications: null, interviews: null};
+
+            fetch(str, {
+                method: "POST",
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(data)
+            });
+
             window.location.replace("../html/index.html");
+        } else {
+            console.log("Account exsists");
+            document.getElementById("registerError").classList.remove("hide");
         }
     })
+}
 });
 
 document.getElementById("loginButton").addEventListener("click", () => {
