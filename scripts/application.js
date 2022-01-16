9ff1a03899c6975cdb1cc02a7fd86adfebc34276
Eric Chen <75589433+EricChen23@users.noreply.github.com>
2022-01-15 21:19:03 -0800

    fixed add

--- scripts/application.js
+++ scripts/application.js
@@ -1,17 +1,16 @@
 document.addEventListener("DOMContentLoaded", ()=> {
     const add = document.querySelector(".add");
-    const cancel = document.querySelector(".cancel");
     const push = document.querySelector(".push");
     var selection = document.querySelector(".selection");
 
     add.addEventListener('click', () => {
         if (selection.style.display === 'none') {
             selection.style.display = 'inline';
+            add.innerHTML = "Cancel";
+        } else {
+            selection.style.display = 'none';
+            add.innerHTML = "Add new application";
         }
-    });
-
-    cancel.addEventListener('click', () => {
-        selection.style.display = 'none';
     });
 
     push.addEventListener('click', () => {
