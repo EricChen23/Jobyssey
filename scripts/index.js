d761625c6e78d7f89ae29607afcc930cf6d976b6
Eric Chen <75589433+EricChen23@users.noreply.github.com>
2022-01-16 04:28:56 -0800

    fixed application

--- scripts/index.js
+++ scripts/index.js
@@ -4,6 +4,8 @@ document.addEventListener("DOMContentLoaded", ()=> {
     const displayDiv = document.querySelector(".display");
     var selection = document.querySelector(".selection");
     const error = document.querySelector(".comp_error");
+    
+    var arr = [];
 
     add.addEventListener('click', () => {
         if (selection.style.display === 'none') {
@@ -20,12 +22,19 @@ document.addEventListener("DOMContentLoaded", ()=> {
     push.addEventListener('click', () => {
         const comsel = document.querySelector(".cominput");
         const comp = comsel.value.toUpperCase();
+        var appCount = 0;
+        var interviewCount = 0;
+        
 
         if (comp === "") {
             error.style.display = 'inline';
             error.innerHTML = "Company name cannot be empty";
             return;
         }
+
+
+        const appCountID = comp.concat("app");
+        const intCountID = comp.concat("int");
 
         fetch('https://jobysseyapi.herokuapp.com/api/v1/company/getCompany').then(response=>{
             return response.json();
@@ -39,10 +48,14 @@ document.addEventListener("DOMContentLoaded", ()=> {
                     name = name.toUpperCase();
                     if (name.localeCompare(comp) == 0) {
                         exist = true;
+                        appCount = obj.applications;
+                        interviewCount = obj.interviews;
                         break;
                     }
                 }
             }
+
+            var displayed = false;
 
             if (exist) {
                 var str = "https://jobysseyapi.herokuapp.com/api/v1/company/".concat(comp, "/0");
@@ -50,6 +63,14 @@ document.addEventListener("DOMContentLoaded", ()=> {
                     method: "POST",
                     headers: {'Content-Type': 'application/json'}
                 });
+                for(var i = 0; i < arr.length; i++) {
+                    if (arr[i].toUpperCase().localeCompare(comp) == 0) {
+                        displayed = true;
+                        const a = document.getElementById(appCountID);
+                        a.innerHTML = "Number of Applications: ".concat(appCount + 1);
+                        break;
+                    }
+                }
             } else {
                 var str = "https://jobysseyapi.herokuapp.com/api/v1/company/company/";
 
@@ -60,24 +81,35 @@ document.addEventListener("DOMContentLoaded", ()=> {
                     headers: {'Content-Type': 'application/json'},
                     body: JSON.stringify(data)
                 });
+                appCount = 1;
+            }
 
+            if (!displayed) {
+                arr.push(comp);
                 selection.style.display = 'none';
                 add.innerHTML = "Add new application";
         
                 const newDiv = document.createElement("div");
                 const newComp = document.createElement("p");
+                const numApp = document.createElement("p");
+                const numInt = document.createElement("p");
                 const interview = document.createElement("p");
                 const buttons = document.createElement("div");
                 const checkBtn = document.createElement("button");
                 const crossBtn = document.createElement("button");
-        
+                
+
                 newComp.innerHTML = comp;
+                numApp.innerHTML = "Number of Applications: ".concat(appCount);
+                numInt.innerHTML = "Number of Interviews: ".concat(interviewCount);
                 interview.innerHTML = "Interview?"
         
                 
                 buttons.appendChild(checkBtn);
                 buttons.appendChild(crossBtn);
                 newDiv.appendChild(newComp);
+                newDiv.appendChild(numApp);
+                newDiv.appendChild(numInt);
                 newDiv.appendChild(interview);
                 newDiv.appendChild(buttons);
         
@@ -88,6 +120,15 @@ document.addEventListener("DOMContentLoaded", ()=> {
                 interview.classList.add("inter");
                 newComp.classList.add("newComp");
                 newDiv.classList.add('display-cell');
+
+                numApp.setAttribute('id', appCountID);
+                numInt.setAttribute('id', intCountID);
+
+                checkBtn.setAttribute('id', comp);
+                crossBtn.setAttribute('id', comp);
+
+                const newDivAttr = comp.concat("id");
+                newDiv.setAttribute('id', newDivAttr);
                 displayDiv.appendChild(newDiv);
             }
         })
